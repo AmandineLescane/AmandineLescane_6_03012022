6f115a1734f34417189d07ded88311b112a9dbd3
Amandine Lescane <amandine.lescane@gmail.com>
2022-01-31 17:51:25 +0100

    Correction bug like/dislike sauces, ajout else/if dans la fonction login et ajout token dans auth.js

--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -1,6 +1,18 @@
 const Sauces = require('../models/sauces');
 const fs = require('fs');
 
+exports.getAllSauces = (req, res, next) => {
+    Sauces.find()
+        .then(sauces => res.status(200).json(sauces))
+        .catch(error => res.status(400).json({ error }));
+};
+
+exports.getOneSauce = (req, res, next) => {
+    Sauces.findOne({ _id: req.params.id })
+        .then(sauces => res.status(200).json(sauces))
+        .catch(error => res.status(404).json({ error }));
+};
+
 exports.createSauce = (req, res, next) => {
     const saucesObject = JSON.parse(req.body.sauce)
     delete saucesObject._id;
@@ -41,17 +53,7 @@ exports.deleteSauce =  (req, res, next) => {
     
 };
 
-exports.getAllSauces = (req, res, next) => {
-    Sauces.find()
-        .then(sauces => res.status(200).json(sauces))
-        .catch(error => res.status(400).json({ error }));
-};
 
-exports.getOneSauce = (req, res, next) => {
-    Sauces.findOne({ _id: req.params.id })
-        .then(sauces => res.status(200).json(sauces))
-        .catch(error => res.status(404).json({ error }));
-};
 
 exports.likeSauce = (req, res, next) => {
     if (req.body.like === 1) {  
@@ -66,11 +68,11 @@ exports.likeSauce = (req, res, next) => {
         Sauces.findOne({ _id: req.params.id })
             .then(sauce => {
             if (sauce.usersLiked.includes(req.body.userId)) {
-                Sauce.updateOne( {_id:req.params.id}, { $pull: { usersLiked: req.body.userId }, $inc: { likes: -1 } })
+                Sauces.updateOne( {_id:req.params.id}, { $pull: { usersLiked: req.body.userId }, $inc: { likes: -1 } })
                 .then(() => res.status(200).json({ message: 'Like supprimé !'}))
                 .catch(error => res.status(400).json({ error }))
             } else if (sauce.usersDisliked.includes(req.body.userId)) {
-                Sauce.updateOne( {_id:req.params.id}, { $pull: { usersDisliked: req.body.userId }, $inc: { dislikes: -1 } })
+                Sauces.updateOne( {_id:req.params.id}, { $pull: { usersDisliked: req.body.userId }, $inc: { dislikes: -1 } })
                 .then(() => res.status(200).json({ message: 'Dislike supprimé !'}))
                 .catch(error => res.status(400).json({ error }))
             }
--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,15 +1,15 @@
 const User = require('../models/user');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-//const cryptojs = require('crypto-js');
+const cryptojs = require('crypto-js');
 require('dotenv').config();
 
 exports.signup = (req, res, next) => {
-    //const emailCrypt = cryptojs.SHA256(req.body.email, `${process.env.CRYPTOJS_SECRET_TOKEN}`).toString();
+    const emailCrypt = cryptojs.SHA256(req.body.email, `${process.env.CRYPTOJS_SECRET_TOKEN}`).toString();
     bcrypt.hash(req.body.password, 10)
     .then(hash => {
         const user = new User({
-            email : req.body.email,
+            email : emailCrypt,
             password: hash
         });
         user.save()
@@ -20,27 +20,31 @@ exports.signup = (req, res, next) => {
 };
 
 exports.login = (req, res, next) => {
-    //const emailCrypt = cryptojs.SHA256(req.body.email, 'process.env.CRYPTOJS_SECRET_TOKEN').toString();
-    User.findOne({ email : req.body.email })
-        .then(user => {
+    const emailCrypt = cryptojs.SHA256(req.body.email, `${process.env.CRYPTOJS_SECRET_TOKEN}`).toString();
+    User.findOne({ email : emailCrypt })
+        .then((user) => {
             if(!user) {
                 return res.status(401).json({message : "Utilisateur non trouvé!"});
-            }
-            bcrypt.compare(req.body.password, user.password)
-                .then(valid => {
+            } 
+            else {
+                bcrypt.compare(req.body.password, user.password)
+                .then((valid) => {
                     if(!valid) {
                         return res.status(401).json({message : "Mot de passe incorrect !"});
                     }
+                    else {
                     res.status(200).json({
                         userId : user._id,
                         token: jwt.sign(
                             { userId: user._id },
-                            process.env.CRYPTOJS_SECRET_TOKEN,
+                            `${process.env.CRYPTOJS_SECRET_TOKEN}`,
                             { expiresIn: '24h' }
-                        )
+                            )
                     });
+                    }
                 })
-                .catch(error => res.status(500).json({ error }));
+                .catch(error => res.status(501).json({ error }));
+            }
         })
         .catch(error => res.status(500).json({ error }));
 };
